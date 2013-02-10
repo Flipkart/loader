7e3492f9a7146b3eadac68533679ad092641d316
Nitin Kumar Agarwal <coolnitincool@yahoo.com>
2013-02-10 17:00:54 +0530

    Added Mysql Connection Collector. Can be used to collect metrics about number of connections being used

---
+++ loader-monitoring/src/main/java/loader/monitor/util/SQLHelper.java
@@ -0,0 +1,104 @@
+package loader.monitor.util;
+
+import org.apache.log4j.Logger;
+import java.sql.*;
+
+public class SQLHelper
+{
+    private Connection con = null;
+    private Statement stm = null;
+    private static Logger logger;
+
+    static {
+        logger = Logger.getLogger(SQLHelper.class);
+    }
+    /**
+     *
+     * @param hostName
+     * @param user
+     * @param password
+     * @param defaultDB
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public SQLHelper(String hostName, int port, String user, String password, String defaultDB) throws ClassNotFoundException, SQLException {
+        String conString = "";
+        Class.forName("com.mysql.jdbc.Driver");
+        if(password==null)
+            password="";
+        conString = "jdbc:mysql://"+hostName+":"+ port;
+        if(defaultDB != null && !defaultDB.trim().equals(""))
+            conString += "/"+defaultDB;
+        conString += "?user="+user+"&password="+password+"&useUnicode=true&characterEncoding=UTF8";
+        logger.debug("Connection String:'"+conString+"'");
+        try{
+            this.con = DriverManager.getConnection(conString);
+        }
+        catch (Exception e) {
+            logger.info("Eating SQL Exception. Test");
+            this.con = DriverManager.getConnection(conString);
+        }
+        this.stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+    }
+
+    /**
+     *
+     * @param query Execute Query and return the result set.
+     * @return Returns ResultSet
+     * @throws SQLException
+     */
+    public ResultSet executeQuery(String query) throws SQLException {
+        logger.debug("Query :'"+query+"'");
+        ResultSet rs = stm.executeQuery(query);
+        return rs;
+    }
+
+    /**
+     *
+     * @param tableName
+     * @param condition
+     * @return Returns number of records with specified condition on the table
+     * @throws SQLException
+     */
+    public long recordInTable(String tableName,String condition) throws SQLException {
+
+        String query = "Select count(*) from "+tableName;
+        if(condition!=null && condition.trim().equals("")==false)
+            query += " where "+condition;
+        ResultSet rs = executeQuery(query);
+        long records = 0;
+        if(rs.next())
+            records = rs.getInt(1);
+        return records;
+    }
+
+    /**
+     *
+     * @param tableName
+     * @param condition
+     * @param primaryKey
+     * @return Returns number of records with specified condition on the table using Primary Key. Its faster
+     * @throws SQLException
+     */
+    public long recordInTable(String tableName,String condition, String primaryKey) throws SQLException {
+
+        String query = "Select count("+primaryKey+") from "+tableName;
+        if(condition!=null && condition.trim().equals("")==false)
+            query += " where "+condition;
+        ResultSet rs = executeQuery(query);
+        long records = 0;
+        if(rs.next())
+            records = rs.getInt(1);
+        return records;
+    }
+
+    /**
+     * Closes SQL Connection
+     * @throws SQLException
+     */
+    public void closeConnection() throws SQLException {
+        stm.close();
+        con.close();
+   }
+
+}
