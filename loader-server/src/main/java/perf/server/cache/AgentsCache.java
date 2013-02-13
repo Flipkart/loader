45e042cd5225e7ab3bb437a561090ce9244e3753
Nitin Kumar Agarwal <coolnitincool@yahoo.com>
2013-02-13 15:56:28 +0530

    Had Lost Local Commits as my laptop crashed and intellij cleaned the files. Recovering those files from last commits and pushing them

--- loader-server/src/main/java/perf/server/cache/AgentsCache.java
+++ loader-server/src/main/java/perf/server/cache/AgentsCache.java
@@ -1,5 +1,10 @@
 package perf.server.cache;
 
+import perf.server.domain.LoaderAgent;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: nitinka
@@ -8,4 +13,28 @@ package perf.server.cache;
  * To change this template use File | Settings | File Templates.
  */
 public class AgentsCache {
+    private static Map<String, LoaderAgent> agentInfoMap;
+    static {
+        agentInfoMap = new HashMap<String, LoaderAgent>();
+    }
+
+    public static Map<String, LoaderAgent> getAgentInfoMap() {
+        return agentInfoMap;
+    }
+
+    public static void setAgentInfoMap(Map<String, LoaderAgent> agentInfoMap) {
+        AgentsCache.agentInfoMap = agentInfoMap;
+    }
+
+    public static void addAgentInfoMap(LoaderAgent agent) {
+        AgentsCache.agentInfoMap.put(agent.getIp(), agent);
+    }
+
+    public static LoaderAgent getAgentInfo(String ip) {
+        return AgentsCache.agentInfoMap.get(ip);
+    }
+
+    public static LoaderAgent removeAgentInfo(String ip) {
+        return AgentsCache.agentInfoMap.remove(ip);
+    }
 }
--- loader-server/src/main/java/perf/server/resource/AgentResource.java
+++ loader-server/src/main/java/perf/server/resource/AgentResource.java
@@ -7,12 +7,17 @@ import com.ning.http.multipart.StringPart;
 import com.sun.jersey.multipart.FormDataParam;
 import com.yammer.metrics.annotation.Timed;
 import org.apache.log4j.Logger;
+import perf.server.cache.AgentsCache;
 import perf.server.cache.LibCache;
 import perf.server.client.LoaderAgentClient;
 import perf.server.config.AgentConfig;
+import perf.server.domain.LoaderAgent;
 import perf.server.util.FileHelper;
+import perf.server.util.ResponseBuilderHelper;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.io.*;
 import java.util.*;
@@ -32,6 +37,84 @@ public class AgentResource {
     }
 
     @Path("/{agentIPs}/libs/platformLibs")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Timed
+    synchronized public LoaderAgent addAgent(@Context HttpServletRequest request,
+                                             Map registrationParams) throws IOException, ExecutionException, InterruptedException {
+
+        AgentsCache.addAgentInfoMap(new LoaderAgent(request.getRemoteAddr(), registrationParams));
+        return AgentsCache.getAgentInfo(request.getRemoteAddr());
+    }
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    @Timed
+    synchronized public Map<String, LoaderAgent> getAgents()
+            throws IOException, ExecutionException, InterruptedException {
+        return AgentsCache.getAgentInfoMap();
+    }
+
+    @Path("/{agentIp}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    @Timed
+    synchronized public LoaderAgent getAgent(@PathParam("agentIp") String agentIp)
+            throws IOException, ExecutionException, InterruptedException {
+
+        LoaderAgent agent = AgentsCache.getAgentInfo(agentIp);
+        if(agent != null)
+            return agent;
+
+        throw new WebApplicationException(ResponseBuilderHelper.agentNotRegistered(agentIp));
+    }
+
+    @Path("/{agentIp}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @DELETE
+    @Timed
+    synchronized public LoaderAgent deleteAgent(@PathParam("agentIp") String agentIp)
+            throws IOException, ExecutionException, InterruptedException {
+
+        LoaderAgent agent = AgentsCache.removeAgentInfo(agentIp);
+        if(agent != null)
+            return agent;
+
+        throw new WebApplicationException(ResponseBuilderHelper.agentNotRegistered(agentIp));
+    }
+
+    @Path("/{agentIp}/disable")
+    @Produces(MediaType.APPLICATION_JSON)
+    @PUT
+    @Timed
+    synchronized public LoaderAgent disableAgent(@PathParam("agentIp") String agentIp)
+            throws IOException, ExecutionException, InterruptedException {
+
+        LoaderAgent agent = AgentsCache.getAgentInfo(agentIp);
+        if(agent != null)
+            return agent.setDisabled();
+
+        throw new WebApplicationException(ResponseBuilderHelper.agentNotRegistered(agentIp));
+    }
+
+    @Path("/{agentIp}/enable")
+    @Produces(MediaType.APPLICATION_JSON)
+    @PUT
+    @Timed
+    synchronized public LoaderAgent enableAgent(@PathParam("agentIp") String agentIp)
+            throws IOException, ExecutionException, InterruptedException {
+
+        LoaderAgent agent = AgentsCache.getAgentInfo(agentIp);
+        if(agent != null)
+            return agent.setEnabled();
+
+        throw new WebApplicationException(ResponseBuilderHelper.agentNotRegistered(agentIp));
+    }
+
+
+
+    @Path("/{agentIPs}/libs/platformLibs")
     @POST
     @Timed
     synchronized public void deployPlatformLib(
@@ -48,7 +131,7 @@ public class AgentResource {
        -F "classList=@Path-To-File-Containing-Class-Names-Separated-With-New-Line"
        http://localhost:9999/loader-server/agents/{comma separated ips}/libs/classLibs
 */
-    
+
     @Path("/{agentIPs}/libs/classLibs")
     @POST
     @Timed
