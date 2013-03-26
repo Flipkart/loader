31c30e2156588473b407e315198bd477a2f10633
Nitin Kumar Agarwal <nitinka@flipkart.com>
2013-03-26 15:42:20 +0530

    Adding project including Http Perf Operations

---
+++ loader-http-operations/src/main/java/perf/operation/http/run/HttpGetRunTestClass.java
@@ -0,0 +1,29 @@
+package perf.operation.http.run;
+
+import com.open.perf.domain.Group;
+import com.open.perf.domain.GroupFunction;
+import com.open.perf.domain.Loader;
+import perf.operation.http.HttpGet;
+
+import java.util.UUID;
+
+public class HttpGetRunTestClass {
+    public static void main (String[]args) throws Exception {
+        new Loader("HttpGetRun").
+                setJobId(UUID.randomUUID().toString()).
+                addGroup(
+                        new Group("HttpGet").
+                                setGroupStartDelay(0).
+                                setRepeats(Integer.parseInt(args[0])).
+                                setThreads(Integer.parseInt(args[1])).
+                                setThroughput(Integer.parseInt(args[2])).
+                                addFunction(new GroupFunction("HttpGet").
+                                        setFunctionClass(HttpGet.class.getCanonicalName()).
+                                        addParam(HttpGet.IP_PARAM_URL,"http://localhost:9999/loader-server/agents").
+                                        addParam(HttpGet.IP_PASS_ON_BODY, false).
+                                        addParam(HttpGet.IP_EXPECTED_STATUS_CODE, 200).
+                                        dumpData())).
+                start();
+
+    }
+}
