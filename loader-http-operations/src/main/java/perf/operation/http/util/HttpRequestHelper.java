2b61cdb5d225a80b9423ef37469056fce4870080
Nitin Kumar Agarwal <coolnitincool@yahoo.com>
2013-04-11 23:51:18 +0530

    Refactored and consolidated common code in http get performance operation so that other http get functions would have little less overlapping code. Added Http Delete performance function

--- loader-http-operations/src/main/java/perf/operation/http/util/HttpRequestHelper.java
+++ loader-http-operations/src/main/java/perf/operation/http/util/HttpRequestHelper.java
@@ -1,18 +1,46 @@
 package perf.operation.http.util;
 
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.Cookie;
-import com.ning.http.client.ProxyServer;
+import com.ning.http.client.*;
 import com.open.perf.core.FunctionContext;
+import com.open.perf.function.FunctionParameter;
 import org.codehaus.jackson.map.ObjectMapper;
 import perf.operation.http.constant.Constants;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class HttpRequestHelper {
     private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static AsyncHttpClient buildClient() {
+        AsyncHttpClientConfig.Builder builder = new AsyncHttpClientConfig.Builder();
+        builder.setAllowPoolingConnection(true).
+                setMaximumConnectionsTotal(1000).
+                setCompressionEnabled(true).
+                setRequestTimeoutInMs(120000);
+        return new AsyncHttpClient(builder.build());
+    }
+
+    public static Response executeRequest(FunctionContext context, AsyncHttpClient.BoundRequestBuilder requestBuilder) throws IOException, InterruptedException, ExecutionException {
+        context.startMe();
+        Future<Response> responseF = requestBuilder.execute();
+        Response response = responseF.get();
+        context.endMe();
+        return response;
+    }
+
+    public static void enhanceRequest(FunctionContext context, AsyncHttpClient.BoundRequestBuilder requestBuilder) throws IOException {
+        addHeaders(context, requestBuilder);
+        addQueryParameters(context, requestBuilder);
+        addParameters(context, requestBuilder);
+        addProxy(context, requestBuilder);
+        setFollowRedirects(context, requestBuilder);
+        addCookies(context, requestBuilder);
+    }
 
     public static void addHeaders(FunctionContext context, AsyncHttpClient.BoundRequestBuilder requestBuilder) throws IOException {
         Map<String, Object> headers = context.getParameterAsMap(Constants.IP_HEADERS);
@@ -66,4 +94,77 @@ public class HttpRequestHelper {
             }
         }
     }
+    
+    public static void addInputCookies(LinkedHashMap<String, FunctionParameter> parameters) {
+        parameters.put(Constants.IP_COOKIES,
+                        new FunctionParameter().
+                                setName(Constants.IP_COOKIES).
+                                setMandatory(false).
+                                setDefaultValue("[]").
+                                setDescription("Proxy for the request as json. Eg : [{'name' : 'value'}]"));
+    }
+
+    public static void addInputProxy(LinkedHashMap<String, FunctionParameter> parameters) {
+        parameters.put(Constants.IP_PROXY,
+                        new FunctionParameter().
+                                setName(Constants.IP_PROXY).
+                                setMandatory(false).
+                                setDefaultValue("{}").
+                                setDescription("Proxy for the request as json. Eg : {'host' : '127.0.0.1', 'port' : '1234'}"));
+    }
+
+    public static void addInputQueryParameters(LinkedHashMap<String, FunctionParameter> parameters) {
+        parameters.put(Constants.IP_QUERY_PARAMETERS,
+                        new FunctionParameter().
+                                setName(Constants.IP_QUERY_PARAMETERS).
+                                setMandatory(false).
+                                setDefaultValue("{}").
+                                setDescription("Http Request Query Parameters as json"));
+    }
+
+    public static void addInputParameters(LinkedHashMap<String, FunctionParameter> parameters) {
+        parameters.put(Constants.IP_PARAMETERS,
+                        new FunctionParameter().
+                                setName(Constants.IP_PARAMETERS).
+                                setMandatory(false).
+                                setDefaultValue("{}").
+                                setDescription("Http Request Parameters as json"));
+    }
+
+    public static void addInputHeaders(LinkedHashMap<String, FunctionParameter> parameters) {
+        parameters.put(Constants.IP_HEADERS,
+                        new FunctionParameter().
+                                setName(Constants.IP_HEADERS).
+                                setMandatory(false).
+                                setDefaultValue("{}").
+                                setDescription("Http Request Headers as json. Eg: {'content-type' : 'application/json', 'accept' : 'application/json'}"));
+    }
+
+    public static void addInputFollowRedirects(LinkedHashMap<String, FunctionParameter> parameters) {
+        parameters.put(Constants.IP_FOLLOW_REDIRECTS,
+                        new FunctionParameter().
+                                setName(Constants.IP_FOLLOW_REDIRECTS).
+                                setMandatory(false).
+                                setDefaultValue(false).
+                                setDescription("If you want to allow request redirects"));
+    }
+
+    public static void addInputExpectedStatusCode(LinkedHashMap<String, FunctionParameter> parameters) {
+        parameters.put(Constants.IP_EXPECTED_STATUS_CODE,
+                        new FunctionParameter().
+                                setName(Constants.IP_EXPECTED_STATUS_CODE).
+                                setMandatory(true).
+                                setDefaultValue(200).
+                                setDescription("Status code to validate"));
+    }
+
+    public static void addInputUrlParam(LinkedHashMap<String, FunctionParameter> parameters) {
+        parameters.put(Constants.IP_PARAM_URL,
+                new FunctionParameter().
+                        setName(Constants.IP_PARAM_URL).
+                        setMandatory(true).
+                        setDefaultValue("").
+                        setDescription("Url to do http Get"));
+    }
+    
 }
---
+++ loader-http-operations/src/main/java/perf/operation/http/util/HttpResponseHelper.java
@@ -0,0 +1,28 @@
+package perf.operation.http.util;
+
+import com.ning.http.client.Response;
+import com.open.perf.core.FunctionContext;
+import org.codehaus.jackson.map.ObjectMapper;
+import perf.operation.http.constant.Constants;
+
+import java.io.IOException;
+
+import static com.open.perf.core.FunctionContext.FailureType.FUNCTIONAL_FAILURE;
+
+public class HttpResponseHelper {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static boolean successfulRequest(FunctionContext context, Response response) {
+        boolean success = (response.getStatusCode() == context.getParameterAsInteger(Constants.IP_EXPECTED_STATUS_CODE));
+        if(!success) {
+            context.failed(FUNCTIONAL_FAILURE,"Status code "+response.getStatusCode());
+        }
+        return success;
+    }
+
+    public static void passOnResponse(FunctionContext context, Response response) throws IOException {
+        if(context.getParameterAsBoolean(Constants.IP_PASS_ON_BODY))
+            context.addParameter(Constants.OP_HTTP_RESPONSE, response.getResponseBody());
+    }
+
+}
