93cbefdf64318ca7da870e861aaae24028a2cdbd
Nitin Kumar Agarwal <coolnitincool@yahoo.com>
2013-04-11 10:58:25 +0530

    Added Constants and Helper class so things can we used across various Http Perf operations

---
+++ loader-http-operations/src/main/java/perf/operation/http/constant/Constants.java
@@ -0,0 +1,20 @@
+package perf.operation.http.constant;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nitinka
+ * Date: 11/4/13
+ * Time: 10:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Constants {
+    public static final String IP_PARAM_URL = "url";
+    public static final String IP_EXPECTED_STATUS_CODE = "expectedStatusCode";
+    public static final String IP_PASS_ON_BODY = "passOnBody";
+    public static final String IP_FOLLOW_REDIRECTS = "followRedirects";
+    public static final String IP_HEADERS = "headers";
+    public static final String IP_PARAMETERS = "parameters";
+    public static final String IP_QUERY_PARAMETERS = "queryParameters";
+    public static final String IP_PROXY = "proxy";
+    public static final String OP_HTTP_RESPONSE = "httpResponse";
+}
--- loader-http-operations/src/main/java/perf/operation/http/util/HttpRequestHelper.java
+++ loader-http-operations/src/main/java/perf/operation/http/util/HttpRequestHelper.java
@@ -1,11 +1,42 @@
 package perf.operation.http.util;
 
-/**
- * Created with IntelliJ IDEA.
- * User: nitinka
- * Date: 10/4/13
- * Time: 10:41 PM
- * To change this template use File | Settings | File Templates.
- */
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.ProxyServer;
+import com.open.perf.core.FunctionContext;
+import perf.operation.http.constant.Constants;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class HttpRequestHelper {
+    public static void addHeaders(FunctionContext context, AsyncHttpClient.BoundRequestBuilder requestBuilder) throws IOException {
+        Map<String, Object> headers = context.getParameterAsMap(Constants.IP_HEADERS);
+        for(String header : headers.keySet()) {
+            requestBuilder.addHeader(header, headers.get(header).toString());
+        }
+    }
+
+    public static void addQueryParameters(FunctionContext context, AsyncHttpClient.BoundRequestBuilder requestBuilder) throws IOException {
+        Map<String, Object> queryParameters = context.getParameterAsMap(Constants.IP_QUERY_PARAMETERS);
+        for(String query : queryParameters.keySet()) {
+            requestBuilder.addQueryParameter(query, queryParameters.get(query).toString());
+        }
+    }
+
+    public static void addParameters(FunctionContext context, AsyncHttpClient.BoundRequestBuilder requestBuilder) throws IOException {
+        Map<String, Object> parameters = context.getParameterAsMap(Constants.IP_PARAMETERS);
+        for(String parameter : parameters.keySet()) {
+            requestBuilder.addParameter(parameter, parameters.get(parameter).toString());
+        }
+    }
+
+    public static void addProxy(FunctionContext context, AsyncHttpClient.BoundRequestBuilder requestBuilder) throws IOException {
+        Map<String, Object> proxyInfoMap = context.getParameterAsMap(Constants.IP_PROXY);
+        requestBuilder.setProxyServer(new ProxyServer(proxyInfoMap.get("host").toString(),
+                Integer.parseInt(proxyInfoMap.get("port").toString())));
+    }
+
+    public static void setFollowRedirects(FunctionContext context, AsyncHttpClient.BoundRequestBuilder requestBuilder) {
+        requestBuilder.setFollowRedirects(context.getParameterAsBoolean(Constants.IP_FOLLOW_REDIRECTS));
+    }
 }
