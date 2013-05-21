5d4f03c3575056a8f42b07867bffedd473d99310
shwet Shashank <shwet.shashank@flipkart.com>
2013-05-21 17:16:28 +0530

    adding mysql resource in monitoringAgent graphs

--- loader-server/src/main/resources/assets/js/monitoringAgentsGraph.js
+++ loader-server/src/main/resources/assets/js/monitoringAgentsGraph.js
@@ -107,6 +107,7 @@ function getResourceType(resource){
 	if(resource.indexOf("memory") !== -1) return "memory";
 	if(resource.indexOf("sockets") !== -1) return "sockets";
 	if(resource.indexOf("diskspace") !== -1) return "diskspace.root";
+	if(resource.indexOf("mysql") !== -1) return "mysql";
 
 }
 
