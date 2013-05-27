cd2449f0d9bf1c7752955ef4b2e923b7df9409d6
shwet Shashank <shwet.shashank@flipkart.com>
2013-05-27 23:04:21 +0530

    added pagination to search jobs page

--- loader-server/src/main/resources/assets/js/search_jobs.js
+++ loader-server/src/main/resources/assets/js/search_jobs.js
@@ -15,14 +15,9 @@ function searchJobs(){
 		dataType:"json",
 		type:"GET",
 		success: function(jobs){
-			var insertHtml = "<table width=\"60%\" border=\"1px\" align=\"center\">";
-			$.each(jobs, function(index, job){
-				console.log(job);
-				insertHtml = insertHtml + "<tr><td><a href=/job_details.html?&jobid=" + job["jobId"] + ">" + job["jobId"] +"</a></td></tr>";
-			});
-			insertHtml = insertHtml + "</table>";
-			$("#jobsList").empty();
-			$("#jobsList").append(insertHtml);
+			window.jobList = jobs;
+			window.nextCounter = 0;
+			displayResults();
 		},
 		error: function(err){
 			console.log(err);
@@ -32,3 +27,40 @@ function searchJobs(){
 		}
 	});
 }
+
+function displayResults(){
+	var begin = window.nextCounter*10;
+	var last = begin + 10 > window.jobList.length?window.jobList.length:begin + 10;
+	var insertHtml = "<table width=\"60%\" border=\"1px\" align=\"center\">";
+			insertHtml = insertHtml + "<thead><tr><td style=\"width:50%;text-align:left\"><button id=\"previousList\" onClick=\"previousList()\">Previous</button></td>";
+			insertHtml = insertHtml + "<td style=\"width:50%;text-align:right\"><button id=\"nextList\" onClick=\"nextList()\">Next</button></td></tr></thead><tbody>";
+			$.each(window.jobList.slice(begin, last), function(index, job){
+				console.log(job);
+				insertHtml = insertHtml + "<tr><td colspan=\"2\"><a href=/job_details.html?&jobid=" + job["jobId"] + ">" + job["jobId"] +"</a></td></tr>";
+			});
+			insertHtml = insertHtml + "</tbody></table>";
+			$("#jobsList").empty();
+			$("#jobsList").append(insertHtml);
+			if (window.nextCounter==0) $("#previousList").attr("disabled","true");
+			if (window.nextCounter==((window.jobList.length/10)>>0)) $("#nextList").attr("disabled","true");
+}
+
+function previousList(){
+	window.nextCounter = window.nextCounter -1;
+	displayResults();
+	if (window.nextCounter==0) $("#previousList").attr("disabled","true");
+	if (window.nextCounter==((window.jobList.length/10)-1)) $("#nextList").removeAttr("disabled");
+}
+
+function nextList(){
+	window.nextCounter = window.nextCounter + 1;
+	displayResults();
+	if (window.nextCounter==1) $("#previousList").removeAttr("disabled");
+	if (window.nextCounter==(window.jobList.length/10)) $("#nextList").attr("disabled","true");
+}
+
+
+
+
+
+
