8b2cca6a6e22c902044e3b79213a9410d686fedb
shwet Shashank <shwet.shashank@flipkart.com>
2013-05-30 15:16:22 +0530

    added counters numbers

--- loader-server/src/main/resources/assets/js/timerGraphs.js
+++ loader-server/src/main/resources/assets/js/timerGraphs.js
@@ -13,6 +13,7 @@ function plotTimerGraphs(){
 				window.stats = new Array();
 				//window.timersToPlot = {};
 				window.timerUrls = new Array();
+				window.counterUrls = new Array();
 				for( var i=0; i<groups.length; i++){
 					var groupJson = groups[i];
 					var insertHtml = "";
@@ -22,14 +23,21 @@ function plotTimerGraphs(){
 							"<span></span></h3>" + 
 							"<div id=\"section" + i + "Container\" class=\"container\" style:\"position:relative\">";
 						window.timerLengths[i] = timers.length;
+						window.counterUrls[i] = new Array();
 						for( var j=0; j<timers.length; j++){
 							var timerName = timers[j]["name"];
 							window.timerUrls.push("/loader-server/jobs/" + jobId + "/jobStats/groups/" + groupJson["groupName"] + "/timers/" + timerName + "/agents/combined") ;
+							window.counterUrls[i].push({
+								"count":"/loader-server/jobs/" + jobId + "/jobStats/groups/" + groupJson["groupName"] + "/counters/" + timerName + "_count/agents/combined?last=true",
+								"error":"/loader-server/jobs/" + jobId + "/jobStats/groups/" + groupJson["groupName"] + "/counters/" + timerName + "_error/agents/combined?last=true",
+								"skip":"/loader-server/jobs/" + jobId + "/jobStats/groups/" + groupJson["groupName"] + "/counters/" + timerName + "_skip/agents/combined?last=true",
+								"failure":"/loader-server/jobs/" + jobId + "/jobStats/groups/" + groupJson["groupName"] + "/counters/" + timerName + "_failure/agents/combined?last=true"
+							});
 							console.log("timer:" + timerName);
 							var chartId = "chart" + i +j;
 							console.log("chartId:" + chartId);
 							insertHtml = insertHtml + "<div class=\"content\" style:\"position:relative\"><table width=\"100%\">" + 
-							"<tr><td colspan=\"2\">" + timerName + 
+							"<tr><td colspan=\"2\">" + timerName + "</td></tr><tr><td colspan=\"2\"><label style=\"color:#228B22\" id=\"count" + i + j + "\"></label>&nbsp;&nbsp;<label style=\"color:#FF0000\" id=\"error" + i + j + "\"></label>&nbsp;&nbsp;<label style=\"color:#FFA500\" id=\"skip" + i + j + "\"></label>&nbsp;&nbsp;<label style=\"color:#FF4500\" id=\"failure" + i + j +"\"></label>" + 
 							"</td></tr><tr><td><div id=\"" + chartId + "1\" style=\"width:50%;float:left;position:relative\"><svg style=\"height: 350px;min-height:350px\"></svg><div id=\"slider-"+ i + "-" + j +"-0\" class=\"slider\" style=\"width:90%;margin:0 auto;\"></div>" + 
 							"</div><div id=\"" + chartId + "2\" style=\"width:50%;float:left;\"><svg style=\"height: 350px; min-height:350px;\"></svg><div id=\"slider-"+ i + "-" + j +"-1\" class=\"slider\" style=\"width:90%;margin:0 auto;\"></div></div></td></tr></table></div>";
 							//insertHtml = insertHtml + "<div class=\"content\"><p>" + timerName + "</p>";
