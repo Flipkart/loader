d90a71294882a51e7be9eda23b5638e164ca02fb
Nitin Kumar Agarwal <nitinka@flipkart.com>
2013-07-03 11:39:46 +0530

    Adding diamond conf creation in create debian package script

--- scripts/createDebianPackage.sh
+++ scripts/createDebianPackage.sh
@@ -1,6 +1,7 @@
 ##This script needs to executed from within scripts folder only
 echo "mvn clean compile install package first"
 cd ../
+echo "Creating core loader Packages"
 modules=("loader-server" "loader-agent" "monitoring-service")
 for module in "${modules[@]}"
 do
@@ -8,3 +9,13 @@ do
  	./create_${module}_deb.sh	
 	cd -
 done
+
+echo "Creating Diamond conf Packages"
+cd monitoring-service/diamond/DEBIAN/
+for conf in `ls`
+do
+	cd ${conf}
+	./create_${conf}_deb.sh
+	cd -
+done
+
