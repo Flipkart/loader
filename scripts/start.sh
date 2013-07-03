1ed7613e074cd5ea01bf00a18e81a423b0824c2e
Nitin Kumar Agarwal <coolnitincool@yahoo.com>
2013-07-03 11:55:25 +0530

    Fixing start.sh. Removed Snapshot from jar name. Would work on any version now.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 cd ../
 mvn clean compile package;
-java -cp  loader-agent/target/loader-agent-1.0-SNAPSHOT.jar:loader-agent/target/lib/* perf.agent.LoaderAgentService loader-agent/config/loader-agent.yml > log &
+java -cp  loader-agent/target/loader-agent-*.jar:loader-agent/target/lib/* perf.agent.LoaderAgentService loader-agent/config/loader-agent.yml > log &
 sleep 5;
-java -cp monitoring-service/target/monitoring-service-1.0-SNAPSHOT.jar:monitoring-service/target/lib/* server.monitor.MonitoringService monitoring-service/config/monitoring-service.yml > log &
-java -cp loader-server/target/loader-server-1.0-SNAPSHOT.jar:loader-server/target/lib/*:/usr/share/loader-server/platformLibs/* perf.server.LoaderServerService loader-server/config/loader-server.yml > log &
+java -cp monitoring-service/target/monitoring-service-*.jar:monitoring-service/target/lib/* server.monitor.MonitoringService monitoring-service/config/monitoring-service.yml > log &
+java -cp loader-server/target/loader-server-*.jar:loader-server/target/lib/*:/usr/share/loader-server/platformLibs/* perf.server.LoaderServerService loader-server/config/loader-server.yml > log &
 cd -
