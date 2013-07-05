fa95e358d6122bc7bf442f6bddff57099100d84b
Nitin Kumar Agarwal <nitinka@flipkart.com>
2013-07-05 14:22:31 +0530

    platform libs will be auto deployed on server upon debian installation

--- loader-server/DEBIAN/create_loader-server_deb.sh
+++ loader-server/DEBIAN/create_loader-server_deb.sh
@@ -9,8 +9,9 @@ die()
 PACKAGE=loader-server
 LOADER_BASE_ROOT="../../"
 CODE_ROOT="../"
+LOADER_CORE_CODE_ROOT="../../loader-core"
 PACKAGE_ROOT="./loader-server"
-VERSION=0.1.3
+VERSION=0.1.4
 ARCH=all
 
 echo "Pre requisite to create this package : mvn clean compile package install -Dmaven.test.skip=true at loader2.0 level"
@@ -23,6 +24,7 @@ mkdir -p $PACKAGE_ROOT/etc/$PACKAGE
 mkdir -p $PACKAGE_ROOT/DEBIAN
 mkdir -p $PACKAGE_ROOT/usr/share/$PACKAGE/lib
 mkdir -p $PACKAGE_ROOT/usr/share/$PACKAGE/app
+mkdir -p $PACKAGE_ROOT/usr/share/$PACKAGE/platformLibs
 mkdir -p $PACKAGE_ROOT/etc/init.d/
 
 ## Copying content for packaging
@@ -39,6 +41,7 @@ cp $PACKAGE.init $PACKAGE_ROOT/etc/init.d/$PACKAGE
 #### COPY CODE BASE
 cp -R $CODE_ROOT/target/*.jar $PACKAGE_ROOT/usr/share/$PACKAGE/app/
 cp -R $CODE_ROOT/target/lib/*.jar $PACKAGE_ROOT/usr/share/$PACKAGE/lib/
+cp -R $LOADER_CORE_CODE_ROOT/target/platform.zip $PACKAGE_ROOT/usr/share/$PACKAGE/platformLibs/
 
 ## Creating Package
 sed -i "s/<VERSION>/$VERSION/g" $PACKAGE_ROOT/DEBIAN/control
