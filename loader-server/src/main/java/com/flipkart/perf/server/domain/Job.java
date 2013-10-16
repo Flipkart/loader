88657b89ea2a0559061472fcb26f58b74cc4404f
Nitin Kumar Agarwal <coolnitincool@yahoo.com>
2013-10-16 14:00:15 +0530

    Added Histogram computation threads

--- loader-server/src/main/java/com/flipkart/perf/server/domain/Job.java
+++ loader-server/src/main/java/com/flipkart/perf/server/domain/Job.java
@@ -279,6 +279,7 @@ public class Job {
         CounterCompoundThread.instance().removeJob(jobId);
         CounterThroughputThread.instance().removeJob(jobId);
         TimerComputationThread.instance().removeJob(jobId);
+        HistogramComputationThread.instance().removeJob(jobId);
         GroupConfConsolidationThread.instance().removeJob(jobId);
 
         // Remove from Running Jobs File
@@ -335,6 +336,7 @@ public class Job {
             CounterCompoundThread.instance().addJob(jobId);
             CounterThroughputThread.instance().addJob(jobId);
             TimerComputationThread.instance().addJob(jobId);
+            HistogramComputationThread.instance().addJob(jobId);
             GroupConfConsolidationThread.instance().addJob(jobId);
 
             persist();
