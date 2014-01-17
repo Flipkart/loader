570a93ff44b60f6b2e191613af7f2fec7959ee14
Nitin Kumar Agarwal <nitinka@flipkart.com>
2014-01-17 14:54:09 +0530

    Catching Exception whicle converting object to string.

--- loader-core/src/main/java/com/flipkart/perf/core/FunctionContext.java
+++ loader-core/src/main/java/com/flipkart/perf/core/FunctionContext.java
@@ -114,7 +114,13 @@ public class FunctionContext {
         String valueString = value.toString();
 
         if(!(value instanceof String)) {
-            valueString = mapper.writeValueAsString(value).replace("\\\"","");
+            try {
+                valueString = mapper.writeValueAsString(value).replace("\\\"","");
+            }
+            catch(org.codehaus.jackson.map.JsonMappingException e) {
+                logger.warn("Error while converting object to string", e);
+                return value;
+            }
         }
 
         Matcher matcher = variablePattern.matcher(valueString);
